ce3ca65ae6302493d853be16f38e16261a98cef6
kmieshkov <mieshkov.k@gmail.com>
2022-04-06 23:22:53 -0400

    test commit

--- src/Tables.java
+++ src/Tables.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Tables {
+
 	public static void main(String[] args) {
 		System.setProperty("webdriver.chrome.driver", "/Users/kmieshkov/ProgramFiles/chromedriver");
 		WebDriver driver = new ChromeDriver();
